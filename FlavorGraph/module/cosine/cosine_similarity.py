08e5279764c4213548e82630545f87540246b83e
jungh8n <jh981113@naver.com>
2022-07-27 16:07:34 +0900

    [OPS-91] refactor : 로직 모듈화

--- FlavorGraph/module/cosine/cosine_similarity.py
+++ FlavorGraph/module/cosine/cosine_similarity.py
@@ -2,40 +2,58 @@ import json
 import numpy as np
 import csv
 
-layer = "../viewPickle/csp_layer.json"
-
-with open(layer, "rb") as f:
-    data = json.load(f)
-veclst_len = max(list(map(lambda x: int(x), data.keys()))) + 1
-vec_lst = [[] for _ in range(veclst_len)]
-nodes = "../../input/nodes_191120.csv"
-
-f = open(nodes, 'r', encoding='utf-8')
-rdr = csv.reader(f)
-next(rdr)
-
-ing_lst = {}
-for line in rdr:
-    num, name, _, node_type, is_hub = line
-    ing_lst[num] = [name, node_type, is_hub]
-
-target = input("select ingredient ID : ")
-t = data[str(target)]
-
-cos_sim = [0 for _ in range(veclst_len)]
-
 
 def sim(a, b):
     return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
 
-for (k, v) in data.items():
-    if k == target:
-        continue
-    cos_sim[int(k)] = sim(v, t)
+def read_json(filename):
+    with open(filename, "rb") as f:
+        data = json.load(f)
 
-print("similar ingredient")
-print("ID : " + str(np.argmax(cos_sim)))
-print("similarity : " + str(np.max(cos_sim)))
-print("name : " + ing_lst[str(np.argmax(cos_sim))][0])
-print("type : " + ing_lst[str(np.argmax(cos_sim))][1])
+    return data
+
+
+def read_csv(filename):
+    f = open(filename, 'r', encoding='utf-8')
+    rdr = csv.reader(f)
+    next(rdr)
+
+    return rdr
+
+
+def generate_ing_data(data):
+    ing_dict = {}
+    for line in data:
+        num, name, _, node_type, is_hub = line
+        ing_dict[num] = [name, node_type, is_hub]
+
+    return ing_dict
+
+
+def calc_cos_sim(data):
+    veclst_len = max(list(map(lambda x: int(x), data.keys()))) + 1
+    cos_sim = [0 for _ in range(veclst_len)]
+    for (k, v) in data.items():
+        if k == target:
+            continue
+        cos_sim[int(k)] = sim(v, t)
+
+    return cos_sim
+
+
+if __name__ == "__main__":
+    vec_data = read_json("../viewPickle/csp_layer.json")
+    node_data = read_csv("../../input/nodes_191120.csv")
+    ing_data = generate_ing_data(node_data)
+
+    target = input("select ingredient ID : ")
+    t = vec_data[str(target)]
+
+    cos_sim = calc_cos_sim(vec_data)
+
+    print("similar ingredient")
+    print("ID : " + str(np.argmax(cos_sim)))
+    print("similarity : " + str(np.max(cos_sim)))
+    print("name : " + ing_data[str(np.argmax(cos_sim))][0])
+    print("type : " + ing_data[str(np.argmax(cos_sim))][1])
